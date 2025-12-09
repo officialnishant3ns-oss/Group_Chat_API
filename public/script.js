fc798147ac3c9fb8156675526e7f904d3dfa9f42
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-12-09 23:13:14 +0530

    added sending via soket

---
+++ public/script.js
@@ -0,0 +1,42 @@
+
+const socket = io()
+let name
+do {
+    name = prompt("Enter your name:")
+} while (!name)
+
+
+
+const messageContainer = document.querySelector('.messagebox')
+const sendbtn = document.querySelector('#sendbtn')
+const messageinput = document.querySelector('#messageinput')
+
+sendbtn.addEventListener('click', () => {
+    const message = messageinput.value.trim()
+    sendmessage(message)
+})
+
+function sendmessage(message) {
+    if (message === '') return
+    const data = {
+        name: name,
+        message: message
+    }
+    appendMessage(data, 'outgoing')
+
+    socket.emit('message', data)
+    messageinput.value = ''
+}
+
+function appendMessage(data, type) {
+    const messageDiv = document.createElement('div')
+    let classname = type
+    messageDiv.classList.add('message', classname)
+    let markup = `
+        <h4>${data.name}</h4>
+        <p>${data.message}</p>
+    `
+    messageDiv.innerHTML = markup
+    messageContainer.appendChild(messageDiv)
+    messageContainer.scrollTop = messageContainer.scrollHeight
+}
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import http from 'http'
 const app = express()
 const PORT = process.env.PORT || 6000
 const server = http.createServer(app)
+import { Server } from "socket.io";
+
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
@@ -14,4 +16,18 @@ app.use(express.static('public'))
 
 app.get('/v1',(req,res)=>{
     res.sendFile('index.html', { root: 'public' })
+})
+
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('New client connected')  
+    socket.on('message', (data) => {
+        console.log('Message received:', data)
+        socket.broadcast.emit('message', data)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected')
+    })   
 })
